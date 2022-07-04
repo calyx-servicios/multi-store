4a4162b0989fbe27492c5b8d0fed0dfbbeb367bc
cristian <crisdparadiso@gmail.com>
2022-07-04 17:24:45 -0300

    [ODOO-4063][ADD][multi_store_security]

---
+++ multi_store_security/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Multi Store Security",
+    "summary": """
+        This module modifies all multi-store
+        access rules 
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["ParadisoCristian"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "version": "13.0.1.0.0",
+    "depends": [
+        "stock_multi_store_users",
+        "purchase_multi_store",
+        "sale_multi_store",
+    ],
+    'data': [
+        'security/multi_store_scurity.xml',
+    ],
+}
