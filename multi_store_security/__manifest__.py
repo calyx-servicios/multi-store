b763cf08236c4648e077036670b5ee9151c895f2
Marco Oegg <marco.oegg@gmail.com>
2023-04-21 18:14:07 -0300

    [UPD][multi_store_security] - add uninstall hook and dependency to account_multi_store

--- multi_store_security/__manifest__.py
+++ multi_store_security/__manifest__.py
@@ -6,15 +6,17 @@
         access rules 
     """,
     "author": "Calyx Servicios S.A.",
-    "maintainers": ["ParadisoCristian"],
+    "maintainers": ["ParadisoCristian, marcooegg"],
     "website": "http://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Custom",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
+    "uninstall_hook": "uninstall_hook",
     "depends": [
         "stock_multi_store_users",
         "purchase_multi_store",
         "sale_multi_store",
+        "account_multi_store",
     ],
     'data': [
         'security/multi_store_scurity.xml',
